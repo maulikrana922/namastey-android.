4cab1c9caa8fc0f827c7dffb30d9af613b6c669e
Dinesh <dinesh.s@latitudetechnolabs.com>
2020-11-24 16:50:19 +0530

    - Changes in chat adapter

--- app/src/main/java/com/namastey/adapter/ChatAdapter.kt
+++ app/src/main/java/com/namastey/adapter/ChatAdapter.kt
@@ -16,14 +16,14 @@ class ChatAdapter(
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        if (viewType == 1) {
+        return if (viewType == 1) {
             val layoutOne: View = LayoutInflater.from(parent.context)
                 .inflate(R.layout.row_message_received, parent, false)
-            return MessageReceiveViewHolder(layoutOne)
+            MessageReceiveViewHolder(layoutOne)
         } else {
             val layoutTwo: View = LayoutInflater.from(parent.context)
                 .inflate(R.layout.row_message_send, parent, false)
-            return MessageSendProfileViewHolder(layoutTwo)
+            MessageSendProfileViewHolder(layoutTwo)
         }
     }
 
@@ -32,11 +32,11 @@ class ChatAdapter(
         when (position) {
             0 -> {
                 val messageReceiveViewHolder = holder as MessageReceiveViewHolder
-
+                messageReceiveViewHolder.bindReceived(position)
             }
             1 -> {
                 val messageSendProfileViewHolder = holder as MessageSendProfileViewHolder
-
+                messageSendProfileViewHolder.bindSend(position)
 
             }
             else -> return
@@ -53,62 +53,70 @@ class ChatAdapter(
 
     }
 
-    internal class MessageReceiveViewHolder(itemView: View) :
+    inner class MessageReceiveViewHolder(itemView: View) :
         RecyclerView.ViewHolder(itemView) {
-        val llMessageReceived: LinearLayout
-        val tvMessageReceived: TextView
-        val tvMessageReceivedTime: TextView
-        val frameImageReceived: FrameLayout
-        val ivImageReceived: ImageView
-        val tvImageReceivedTime: TextView
-        val llRecordingReceived: LinearLayout
-        val ivRecordReceived: ImageView
-        val tvReadableContentReceived: TextView
-        val tvRecordedTimeReceived: TextView
-        val tvRecordingTimeReceived: TextView
 
-        init {
-            llMessageReceived = itemView.findViewById(R.id.llMessageReceived)
-            tvMessageReceived = itemView.findViewById(R.id.tvMessageReceived)
-            tvMessageReceivedTime = itemView.findViewById(R.id.tvMessageReceivedTime)
-            frameImageReceived = itemView.findViewById(R.id.frameImageReceived)
-            ivImageReceived = itemView.findViewById(R.id.ivImageReceived)
-            tvImageReceivedTime = itemView.findViewById(R.id.tvImageReceivedTime)
-            llRecordingReceived = itemView.findViewById(R.id.llRecordingReceived)
-            ivRecordReceived = itemView.findViewById(R.id.ivRecordReceived)
-            tvReadableContentReceived = itemView.findViewById(R.id.tvReadableContentReceived)
-            tvRecordedTimeReceived = itemView.findViewById(R.id.tvRecordedTimeReceived)
-            tvRecordingTimeReceived = itemView.findViewById(R.id.tvRecordingTimeReceived)
+        fun bindReceived(position: Int) = with(itemView){
+
         }
+//        val llMessageReceived: LinearLayout
+//        val tvMessageReceived: TextView
+//        val tvMessageReceivedTime: TextView
+//        val frameImageReceived: FrameLayout
+//        val ivImageReceived: ImageView
+//        val tvImageReceivedTime: TextView
+//        val llRecordingReceived: LinearLayout
+//        val ivRecordReceived: ImageView
+//        val tvReadableContentReceived: TextView
+//        val tvRecordedTimeReceived: TextView
+//        val tvRecordingTimeReceived: TextView
+//
+//        init {
+//            llMessageReceived = itemView.findViewById(R.id.llMessageReceived)
+//            tvMessageReceived = itemView.findViewById(R.id.tvMessageReceived)
+//            tvMessageReceivedTime = itemView.findViewById(R.id.tvMessageReceivedTime)
+//            frameImageReceived = itemView.findViewById(R.id.frameImageReceived)
+//            ivImageReceived = itemView.findViewById(R.id.ivImageReceived)
+//            tvImageReceivedTime = itemView.findViewById(R.id.tvImageReceivedTime)
+//            llRecordingReceived = itemView.findViewById(R.id.llRecordingReceived)
+//            ivRecordReceived = itemView.findViewById(R.id.ivRecordReceived)
+//            tvReadableContentReceived = itemView.findViewById(R.id.tvReadableContentReceived)
+//            tvRecordedTimeReceived = itemView.findViewById(R.id.tvRecordedTimeReceived)
+//            tvRecordingTimeReceived = itemView.findViewById(R.id.tvRecordingTimeReceived)
+//        }
     }
 
-    internal class MessageSendProfileViewHolder(itemView: View) :
+    inner class MessageSendProfileViewHolder(itemView: View) :
         RecyclerView.ViewHolder(itemView) {
-        val llMessageSend: LinearLayout
-        val tvMessageSend: TextView
-        val tvMessageSendTime: TextView
-        val frameImageSend: FrameLayout
-        val ivImageSend: ImageView
-        val tvImageSendTime: TextView
-        val llRecordingSend: LinearLayout
-        val ivRecordSend: ImageView
-        val tvReadableContentSend: TextView
-        val tvRecordedTimeSend: TextView
-        val tvRecordingTimeSend: TextView
 
-        init {
-            llMessageSend = itemView.findViewById(R.id.llMessageSend)
-            tvMessageSend = itemView.findViewById(R.id.tvMessageSend)
-            tvMessageSendTime = itemView.findViewById(R.id.tvMessageSendTime)
-            frameImageSend = itemView.findViewById(R.id.frameImageSend)
-            ivImageSend = itemView.findViewById(R.id.ivImageSend)
-            tvImageSendTime = itemView.findViewById(R.id.tvImageSendTime)
-            llRecordingSend = itemView.findViewById(R.id.llRecordingSend)
-            ivRecordSend = itemView.findViewById(R.id.ivRecordSend)
-            tvReadableContentSend = itemView.findViewById(R.id.tvReadableContentSend)
-            tvRecordedTimeSend = itemView.findViewById(R.id.tvRecordedTimeSend)
-            tvRecordingTimeSend = itemView.findViewById(R.id.tvRecordingTimeSend)
+        fun bindSend(position: Int) = with(itemView){
+
         }
+//        val llMessageSend: LinearLayout
+//        val tvMessageSend: TextView
+//        val tvMessageSendTime: TextView
+//        val frameImageSend: FrameLayout
+//        val ivImageSend: ImageView
+//        val tvImageSendTime: TextView
+//        val llRecordingSend: LinearLayout
+//        val ivRecordSend: ImageView
+//        val tvReadableContentSend: TextView
+//        val tvRecordedTimeSend: TextView
+//        val tvRecordingTimeSend: TextView
+//
+//        init {
+//            llMessageSend = itemView.findViewById(R.id.llMessageSend)
+//            tvMessageSend = itemView.findViewById(R.id.tvMessageSend)
+//            tvMessageSendTime = itemView.findViewById(R.id.tvMessageSendTime)
+//            frameImageSend = itemView.findViewById(R.id.frameImageSend)
+//            ivImageSend = itemView.findViewById(R.id.ivImageSend)
+//            tvImageSendTime = itemView.findViewById(R.id.tvImageSendTime)
+//            llRecordingSend = itemView.findViewById(R.id.llRecordingSend)
+//            ivRecordSend = itemView.findViewById(R.id.ivRecordSend)
+//            tvReadableContentSend = itemView.findViewById(R.id.tvReadableContentSend)
+//            tvRecordedTimeSend = itemView.findViewById(R.id.tvRecordedTimeSend)
+//            tvRecordingTimeSend = itemView.findViewById(R.id.tvRecordingTimeSend)
+//        }
     }
 
     override fun getItemCount() = 7
