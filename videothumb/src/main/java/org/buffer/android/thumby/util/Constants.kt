caf47ea1d2c950cd173c8da5eaac0778e23e9b5a
Mehul <mehul.p@latitudetechnolabs.com>
2021-08-27 12:34:43 +0530

    Add from gallery flow done

---
+++ videothumb/src/main/java/org/buffer/android/thumby/util/Constants.kt
@@ -0,0 +1,15 @@
+package org.buffer.android.thumby.util
+
+import android.os.Environment
+import java.io.File
+
+object Constants {
+    const val PERMISSION_CAMERA = 102
+    const val REQUEST_POST_VIDEO = 103
+    const val REQUEST_CODE_IMAGE = 104
+    const val IMAGE_PATH = "image"
+    const val IMAGE_TYPE = "image/*"
+    private val ROOT = File.separator + "NAMASTEY"
+    private val SD_CARD_PATH = Environment.getExternalStorageDirectory().path
+    val FILE_PATH = SD_CARD_PATH.plus(ROOT)
+}
